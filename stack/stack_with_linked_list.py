c2eef0e735d4a4d3c41b414e22a4eda2172e6906
timurridjanovic <timur.ridjanovic@gmail.com>
2013-11-26 03:54:58 -0500

    changed stack with linked-list

--- stack/stack_with_linked_list.py
+++ stack/stack_with_linked_list.py
@@ -1,7 +1,31 @@
-from linked_list import LinkedList, Node
+from linked_list.linked_list import LinkedList, Node
 
-newList = LinkedList()
+class Stack(object):
+	def __init__(self):
+		self.stack = LinkedList()
 
-newList.append('hello')
+	def push(self, element):
+		self.stack.append(element)
 
-newList.showList()
+	def pop(self):
+		return self.stack.pop()
+
+	def peek(self):
+		if self.stack.length():
+			return self.stack.retrieve(self.stack.length() - 1)
+		else:
+			raise Exception("List is Empty")
+
+	def isEmpty(self):
+		return not self.stack.length()
+
+	def size(self):
+		return self.stack.length()
+
+
+stack = Stack()
+
+stack.push("Timmy")
+stack.push("hello")
+stack.push("babu")
+print stack.size()
