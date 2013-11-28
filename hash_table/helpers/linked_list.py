6c9921bb3e8db2268a558f4856fe6039e7d3629a
timurridjanovic <timur.ridjanovic@gmail.com>
2013-11-27 19:10:07 -0500

    added features

--- hash_table/helpers/linked_list.py
+++ hash_table/helpers/linked_list.py
@@ -1,6 +1,27 @@
 class LinkedList(object):
-    def __init__(self):
+    def __init__(self, *args):
         self.currentNode = None
+        for e in args:
+            self.append(e)
+            
+    def __iter__(self):
+        for i in range(self.length()):
+            if type(self.retrieve(i)) == LinkedList:
+                yield self.retrieve(i).showList()
+            else:
+                yield self.retrieve(i) 
+                
+    def __getitem__(self, index):
+        return self.retrieve(index)  
+        
+    def __setitem__(self, index, value):
+        self.updateByIndex(value, index)              
+            
+    def __repr__(self):
+        return self.showList() 
+        
+    def __len__(self):
+        return self.length()           
 
     def append(self, element):
         if self.currentNode:
@@ -16,26 +37,29 @@ class LinkedList(object):
         else:
             pop = self.currentNode
             self.currentNode = self.currentNode.parent
-            if self.length() > 1:
+            if self.length() > 0:
                 self.currentNode.child = None              
             return pop.node
 
     def showList(self, reverse=False):
+        listo = '['
         if self.currentNode == None:
-            return None
+            return listo + ']'
         node = self.currentNode
         while node.parent is not None:
-            if reverse is True:
-                print node.node, node.parent.node, node.index
             node = node.parent    
-        if reverse is True:
-            print node.node, node.parent, node.index
-            return
                     
         while node.child is not None:
-            print node.node, node.child.node, node.index
+            if type(node.node) == LinkedList:
+                listo = listo + node.node.showList() + ', '    
+            else:
+                listo = listo + str(node.node) + ', '
             node = node.child
-        print node.node, node.child, node.index
+        if type(node.node) == LinkedList:
+            listo = listo + node.node.showList() + ']'    
+        else:
+            listo = listo + str(node.node) + ']'
+        return listo
         
         
         
@@ -44,30 +68,23 @@ class LinkedList(object):
             self.append(element)
             return
             
-        while self.currentNode.index is not index:
-            self.currentNode = self.currentNode.parent
+        self.getNodeByIndex(index)
         newNode =  Node(element, self.currentNode.parent, self.currentNode, self.currentNode.index)
         
         if self.currentNode.parent:
             self.currentNode.parent.child = newNode
         self.currentNode.parent = newNode
         
-        while self.currentNode.child is not None:
-            self.currentNode.index += 1
-            self.currentNode = self.currentNode.child
-        self.currentNode.index += 1   
+        self.goBackToFrontNode('insert')  
         
         
     def delete(self, index):
-        if self.currentNode == None:
-            raise Exception("List is Empty")
         if index > self.currentNode.index:
             raise Exception("List index is out of range")
         if index == self.currentNode.index:
             self.pop()    
         else:
-            while self.currentNode.index is not index:
-                self.currentNode = self.currentNode.parent
+            self.getNodeByIndex(index)
         
             previous = self.currentNode.parent
             next = self.currentNode.child
@@ -80,45 +97,70 @@ class LinkedList(object):
                 self.currentNode = self.currentNode.child
                 self.currentNode.parent = previous
                 
-            while self.currentNode.child is not None:
-                self.currentNode.index -= 1
-                self.currentNode = self.currentNode.child
-            self.currentNode.index -= 1        
+            self.goBackToFrontNode('delete')     
 
     def find(self, element):
-        if self.currentNode == None:
-            raise Exception("List is Empty")
-        while self.currentNode.node is not element or self.currentNode.parent is not None:
-            self.currentNode = self.currentNode.parent
-            
-        if self.currentNode.node == element:
-            found = self.currentNode.node
+        self.getNode(element)
+        if type(self.currentNode.node) == LinkedList:
+            if element == self.currentNode.node.showList():
+                found = self.currentNode
         else:
-            found = None
-        while self.currentNode.child is not None:
-            self.currentNode = self.currentNode.child
-        return found        
-                        
-            
-    def retrieve(self, index):
+            if self.currentNode.node == element:
+                found = self.currentNode
+            else:
+                found = None
+        self.goBackToFrontNode()
+        return found 
+        
+    def update(self, element, newElement):
+        """
+        updates only one instance of the element in the list. Watch out for duplicates
+        """
+        node = self.find(element)
+        index = node.index
+        self.updateByIndex(newElement, index)
+        
+    def updateByIndex(self, value, index):
+        self.delete(index)
+        self.insert(value, index)   
+           
+    def getNode(self, element):
         if self.currentNode == None:
             raise Exception("List is Empty")    
+        while self.currentNode.node != element and self.currentNode.parent != None:
+            if type(self.currentNode.node) == LinkedList:
+                if element == self.currentNode.node.showList():
+                    return self.currentNode.node.showList()             
+            self.currentNode = self.currentNode.parent
+            
+    def getNodeByIndex(self, index):
+        if self.currentNode == None:
+            raise Exception("List is Empty")
+            
+        while self.currentNode.index is not index:
+            self.currentNode = self.currentNode.parent               
+            
+    def goBackToFrontNode(self, updateIndexes=None):
+        while self.currentNode.child is not None:
+            if updateIndexes == 'delete':
+                self.currentNode.index -= 1  
+            if updateIndexes == 'insert':
+                self.currentNode.index += 1 
+            self.currentNode = self.currentNode.child 
+        if updateIndexes == 'delete':
+            self.currentNode.index -= 1   
+        if updateIndexes == 'insert':
+            self.currentNode.index += 1                                  
+                        
+            
+    def retrieve(self, index):    
         if index > self.currentNode.index:
             raise Exception("Index is out of range")
             
+        self.getNodeByIndex(index)
         node = self.currentNode
-        while node.index is not index:
-            node = node.parent
+        self.goBackToFrontNode()
         return node.node   
-        
-        
-    def loopOverList(self, operation):
-        if self.currentNode == None:
-            raise Exception("List is Empty")
-        length = self.length()
-        for i in range(length-1):
-            operation(self.currentNode)
-            self.currentNode = self.currentNode.parent
             
                
     def length(self):
